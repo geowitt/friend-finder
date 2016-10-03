fdbccafac6d43f9669dea51677d763d7ec0dd9b5
George Witt <gwitt@Georges-MacBook-Pro.local>
2016-10-03 18:04:47 -0400

    updates

---
+++ app/data/friends.js
@@ -0,0 +1,16 @@
+    var friends = [ {
+        "name" : "George",
+        "photo" : "http://getmorepc.com/wp-content/uploads/2011/05/IT-GUY.jpg",
+        "scores" : [5,5,2,3,1,5,2,1,3,5]
+    }, {
+        "name" : "Ethan",
+        "photo" : "http://www.exec-it.com/wp-content/uploads/2013/09/it-guy.jpg",
+        "scores" : [1,5,1,1,1,1,1,1,5,5]
+    }, {
+        "name" : "Tom",
+        "photo" : "https://www.blueclaw.co.uk/wp-content/uploads/2013/10/Nedry-660x358.jpg",
+        "scores" : [5,3,2,3,2,5,1,3,3,4]
+    }];
+
+
+    module.exports = friends;
